c71bd95002d17b18947f1391cde977a3d066e848
Anton <2004lol222@mail.ru>
2023-12-19 23:58:41 +0800

    1 задача

--- example_1/Program.cs
+++ example_1/Program.cs
@@ -1,8 +1,9 @@
-﻿﻿int[] ArrNums(int size, int from, int to){
+﻿//если указывать начала массива из консоли от 100 до 999, то подойдет этот способ
+int[] ArrNums(int size, int from, int to){
     int[] arr = new int[size];
 
     for (int i = 0; i < size; i++){
-        arr[i] = new Random().Next(from, to + 1);
+        arr[i] = new Random().Next(from, to + 1); 
     }
     return arr;
 }
@@ -38,4 +39,44 @@ Console.WriteLine("транслируем массив: ");
 print(mass);
 
 Console.Write($"итог: {hero(mass)}");
-//Console.WriteLine(hero(mass));
+
+
+
+
+// 2 способ
+
+// int[] ArrNums(int size){
+//     int[] arr = new int[size];
+
+//     for (int i = 0; i < size; i++){
+//         arr[i] = new Random().Next(100, 1000);
+//     }
+//     return arr;
+// }
+
+// void print(int[] arr){
+//     int size = arr.Length;
+//     for (int i = 0; i < size; i++){
+//         Console.Write($"{arr[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// int hero(int[] arr){
+//     int y = 0;
+//     for(int i = 0; i < arr.Length; i++){
+//         if (arr[i] % 2  == 0) {
+//             y++;
+//         }
+//     }
+//     return y;
+// }
+
+// Console.WriteLine("Введите размер массива: ");
+// int num = int.Parse(Console.ReadLine()!);
+// int[] mass = ArrNums(num);
+
+// Console.WriteLine("транслируем массив: ");
+// print(mass);
+
+// Console.Write($"итог: {hero(mass)}");
